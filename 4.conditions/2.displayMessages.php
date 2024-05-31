ce1bc1e3066ff8ecb4abb88406b9fad7a3de825e
isab95 <noelisabelle15@gmail.com>
2024-05-31 12:02:59 +0200

    exercise 2, sorry didn't use AND / OR in my condition because I would have had to use it at each condition andI was  lazy

--- 4.conditions/2.displayMessages.php
+++ 4.conditions/2.displayMessages.php
@@ -4,8 +4,24 @@
     </head>
     <body>
         <?php
-
-        ?>
-        
+            $greeting = ["Good morning !", "Good day !", "Good afternoon !", "Good evening", "Good night"];
+            $now = date('H:i:s');
+            $hour = (int) date('H');
+            if ($hour >= 5){
+                if ($hour <= 9){
+                    echo $greeting[0];
+                }elseif ($hour <= 12){
+                    echo $greeting[1];
+                }elseif ($hour <= 16){
+                    echo $greeting[2];
+                }elseif ($hour <= 21){
+                    echo $greeting[3];
+                }else{
+                    echo $greeting[4]; 
+                }
+            }else {
+                echo $greeting[4];
+            }
+        ?>  
     </body>
 </html>
