2222ac61262c641744b33af990631c033365c2b5
isab95 <noelisabelle15@gmail.com>
2024-05-31 11:33:06 +0200

    finish exercise 1.2

--- 4.conditions/1.cleanYourRoom.php
+++ 4.conditions/1.cleanYourRoom.php
@@ -4,12 +4,13 @@
     </head>
     <body>
         <?php
-            $room_is_filthy = true;
-            if($room_is_filthy){
-                echo "Yuk, Room is dirty : let's clean it up !";
+            $possible_states = ['filthy', 'dirty', 'clean'];
+            $room_filthiness = 0;
+            if($room_filthiness < (count($possible_states)-1)){
+                echo "Yuk, Room is $possible_states[$room_filthiness] : let's clean it up !";
 	            // cleanup_room();
 	            echo "<br>Room is now clean!";
-	            $room_is_filthy = false;
+	            $possible_states = 2;
             } else {
 	            echo "<br>Nothing to do, room is neat.";
             }
